1ac0271c86adc39e297083f0823729b99488c947
Robert Stoll <rstoll@tutteli.ch>
2013-10-20 22:11:57 +0200

    Moved ErrorReporterHelper from typechecker to common

---
+++ src/ch/tutteli/tsphp/common/ErrorReporterHelper.java
@@ -0,0 +1,26 @@
+package ch.tutteli.tsphp.common;
+
+import ch.tutteli.tsphp.common.exceptions.TSPHPException;
+import org.antlr.runtime.RecognitionException;
+
+import java.util.Collection;
+
+public final class ErrorReporterHelper
+{
+    private ErrorReporterHelper() {
+    }
+
+    public static void reportError(
+            final Collection<IErrorLogger> errorLoggers,
+            final RecognitionException exception,
+            final String phase) {
+
+        final String tokenText = exception.token != null
+                ? "Unexpected token: " + exception.token.getText()
+                : "Unknown token";
+        for (IErrorLogger logger : errorLoggers) {
+            logger.log(new TSPHPException("Line " + exception.line + "|" + exception.charPositionInLine
+                    + " exception during " + phase + " phase occurred. " + tokenText, exception));
+        }
+    }
+}
