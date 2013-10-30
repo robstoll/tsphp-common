f9d51a9193f713184987dcb36e4e9b0afaf24780
Robert Stoll <rstoll@tutteli.ch>
2013-10-30 19:37:29 +0100

    TSPHP-632 UnexpectedTokenException for all kind of parsing errors

---
+++ src/ch/tutteli/tsphp/common/exceptions/UnexpectedTokenException.java
@@ -0,0 +1,24 @@
+package ch.tutteli.tsphp.common.exceptions;
+
+import org.antlr.runtime.RecognitionException;
+
+public class UnexpectedTokenException extends TSPHPException
+{
+    private final RecognitionException recognitionException;
+    private final String phase;
+
+    public UnexpectedTokenException(String message, RecognitionException theRecognitionException, String thePhase) {
+        super(message);
+        recognitionException = theRecognitionException;
+        phase = thePhase;
+    }
+
+    public RecognitionException getRecognitionException() {
+        return recognitionException;
+    }
+
+    public String getPhase() {
+        return phase;
+    }
+}
+
